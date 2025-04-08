c661a7acdd62367d41f37a147c7b526ddba89c2d
Cam Shinker <shinkecj@mail.uc.edu>
2025-04-08 08:35:10 -0400

    Add project files.

---
+++ GregGraham_Assignment11/function_package/functions.py
@@ -0,0 +1,13 @@
+# File Name: functions.py
+# Student Name: Cam Shinker, Luke elmore
+# email: shinkecj@mail.uc.edu, elmorels@mail.uc.edu
+# Assignment Number: Assignment 11
+# Due Date: 4/17/2025
+# Course #/Section: IS 4010-002
+# Semester/Year: Spring 2025
+# Brief Description of the assignment: Clean some messy CSV data
+
+# Brief Description of what this module does: Contains a class with functions to perform the assignment
+# Citations: 
+
+# Anything else that's relevant:
---
+++ GregGraham_Assignment11/main_package/main.py
@@ -0,0 +1,17 @@
+# File Name: main.py
+# Student Name: Cam Shinker, Luke elmore
+# email: shinkecj@mail.uc.edu, elmorels@mail.uc.edu
+# Assignment Number: Assignment 11
+# Due Date: 4/17/2025
+# Course #/Section: IS 4010-002
+# Semester/Year: Spring 2025
+# Brief Description of the assignment: Clean some messy CSV data
+
+# Brief Description of what this module does: Contains the entry point code for the assignment
+# Citations: 
+
+# Anything else that's relevant:
+
+
+if __name__ == "__main__":
+    pass
